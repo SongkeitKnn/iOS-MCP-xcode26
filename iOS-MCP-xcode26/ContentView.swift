2e55176190b1839974e7534a9814651056d3430b
songkeik-alt <songkeit.k@arise.tech>
2026-05-17 13:40:17 +0700

    Initial Commit

---
+++ iOS-MCP-xcode26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-MCP-xcode26
+//
+//  Created by 667208 on 17/5/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-MCP-xcode26/iOS_MCP_xcode26App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_MCP_xcode26App.swift
+//  iOS-MCP-xcode26
+//
+//  Created by 667208 on 17/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS_MCP_xcode26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
